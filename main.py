eac311196dd4fd4458fe1cee761e6b757656183c
bryan puchaicela <bryapuch@espol.edu.ec>
2021-11-13 15:09:48 -0500

    add example in file txt, tokens are Identificador, commit, ignore, boolean,coma,newline

--- main.py
+++ main.py
@@ -5,54 +5,73 @@ import ply.lex as lex
 # List of token names.   This is always required
 
 #Stefany Lavayen: palabras reservadas 
-keywords = {
-  'break' : 'BREAK',
-  'else' : 'ELSE',
-  'for' : 'FOR',
-  'if' : 'IF',
-  'case' : 'CASE',
-  'func' : 'FUNC',
-  'continue' : 'CONTINUE',
+reserved = {
+  'break'       : 'BREAK',
+  'else'        : 'ELSE',
+  'for'         : 'FOR',
+  'if'          : 'IF',
+  'case'        : 'CASE',
+  'func'        : 'FUNC',
+  'continue'    : 'CONTINUE',
   'fallthrough' : 'FALLTHROUGH',
-  'goto' : 'GOTO',
-  'interface' : 'INTERFACE',
-  'map' : 'MAP',
-  'package' : 'PACKAGE',
-  'range' : 'RANGE',
-  'return' : 'RETURN',
-  'select' : 'SELECT',
-  'struct' : 'STRUCT',
-  'switch' : 'SWITCH',
-  'type' : 'TYPE',
-  'var' : 'VAR',
-  'chan' : 'CHAN', 
-  'go' : 'GO',
-  'default': 'DEFAULT',
-  'defer' : 'DEFER',
-  'import' : 'IMPORT',
-  'const' : 'CONST'
+  'goto'        : 'GOTO',
+  'interface'   : 'INTERFACE',
+  'map'         : 'MAP',
+  'package'     : 'PACKAGE',
+  'range'       : 'RANGE',
+  'return'      : 'RETURN',
+  'select'      : 'SELECT',
+  'struct'      : 'STRUCT',
+  'switch'      : 'SWITCH',
+  'type'        : 'TYPE',
+  'var'         : 'VAR',
+  'chan'        : 'CHAN', 
+  'go'          : 'GO',
+  'default'     : 'DEFAULT',
+  'defer'       : 'DEFER',
+  'import'      : 'IMPORT',
+  'const'       : 'CONST'
 }
 
 tokens = (
+  'IDENTIFICADOR',
   'NUMBER',
   'PARLEFT',
   'PARRIGHT',
   'MAYORQUE',
   'MENORQUE',
-  'IGUAL'
-) + tuple(keywords.values())
+  'IGUAL',
+  'COMENTARIO',
+  'COMA',
+  'BOOLEAN'
+ ) + tuple(reserved.values())
 
+# IDENTIFICADOR
+def t_IDENTIFICADOR(t):
+    r'[a-zA-Z_][A-Za-z0-9_]*'
+    t.type = reserved.get(t.value,'IDENTIFICADOR') 
+    return t
 # Regular expression rules for simple tokens
-t_PARLEFT = r'\('
+t_PARLEFT  = r'\('
 t_PARRIGHT = r'\)'
 t_MAYORQUE = r'>'
 t_MENORQUE = r'<'
-t_IGUAL = r'='
+t_IGUAL    = r'='
+t_COMA     = r','
+t_BOOLEAN  = r'true|false'
+t_IF       = r'(if)'
+# start_comentario
+def t_COMENTARIO(t):
+  r'(\//.*|\/\*.*\*\/)'
+  pass
+# end_comentario
 
-t_IF=r'(if)'
 
-
-#Algorithm
+#IGNORE
+t_ignore = ' \t'
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
 def t_error(t):
   print("No se reconoce '%s'" % t.value[0])
   t.lexer.skip(1)
