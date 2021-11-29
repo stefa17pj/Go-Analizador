fe2c8fe20d252af7e6a2a616a7f38a9e3f131314
stefany <stelsant@espol.edu.ec>
2021-11-29 16:39:01 -0500

    interfaz base

--- interfaz.py
+++ interfaz.py
@@ -4,35 +4,39 @@ from analizadorSintactico import analysisSyntax
 
 
 def getTextInputLex():
-     text= textCode.get(1.0,END)
-     LabelResultado["text"] = analysisLex(text)
+    text= textCode.get(1.0,END)
+    LabelResultado["text"] = analysisLex(text)
 
 def getTextInputSin():
-     text = textCode.get(1.0, END)
-     LabelResultado["text"] = analysisSyntax(text)
+    text = textCode.get(1.0, END)
+    LabelResultado["text"] = analysisSyntax(text)
+
+def clear():
+    textCode.delete(1.0, END)
 
 #Interfaz Base
-raiz =Tk()
-raiz.title("Analizador GO")
-raiz.resizable(0,0)
-miFrame = Frame()
-miFrame.config(width="700", height="400", bg="gray")
-miFrame.pack()
-frameResult =  Frame()
-frameResult.config(width='700', height='400')
-frameResult.pack()
-Label(miFrame, text="Ingrese el texto para el analisis", fg="black", bg="gray").place(x=325, y=50, anchor="center")
-
+root =Tk()
+root.title("Analizador GO")
+root.resizable(0,0)
+root.geometry("600x600")
 
 # TEXTAREA
-textCode = Text(miFrame, width=70, height=15, padx=10, pady=10)
-textCode.place(x=325, y=200, anchor="center")
-LabelResultado= Label(frameResult, fg="black", bg = "white",font=("Arial Novas", 12))
-LabelResultado.place(x=325, y=200, anchor="center")
+Label1 = Label(root, text="Analizador Sintactico de GO")
+Label1.pack(pady=20)
+textCode = Text(root, width=40, height=10)
+textCode.pack(pady=20)
 
 # BOTONES
-botonLexico = Button(frameResult, text="Analizador Lexico",command=getTextInputLex).place(x=100, y=50)
-botonSintactico = Button(frameResult, text="Analizador Sintactico",command=getTextInputSin).place(x=400, y=50)
+buttonFrame = Frame(root)
+buttonFrame.pack()
+botonLimpiar = Button(buttonFrame, text="Limpiar", command=clear)
+botonLimpiar.grid(row=0, column=0)
+botonLexico = Button(buttonFrame, text="Lexico",command=getTextInputLex)
+botonLexico.grid(row=0, column=1, padx=20)
+botonSintactico = Button(buttonFrame, text="Sintactico",command=getTextInputSin)
+botonSintactico.grid(row=0, column=2, padx=10)
 
+LabelResultado = Label(root, text ='')
+LabelResultado.pack(pady=20)
 
-raiz.mainloop()
+root.mainloop()
