30f31bc62ca6ce913526d569b8e94f6c3f68f47a
stefany <stelsant@espol.edu.ec>
2021-11-29 16:07:14 -0500

    cambios

--- interfaz.py
+++ interfaz.py
@@ -5,7 +5,7 @@ from analizadorSintactico import analysisSyntax
 
 def getTextInputLex():
      text= textCode.get(1.0,END)
-     LabelResultado["text"]= analysisLex(text)
+     LabelResultado["text"] = analysisLex(text)
 
 def getTextInputSin():
      text = textCode.get(1.0, END)
@@ -15,22 +15,24 @@ def getTextInputSin():
 raiz =Tk()
 raiz.title("Analizador GO")
 raiz.resizable(0,0)
-miFrame = Frame(raiz, width="650", height="700", bg="lightblue")
+miFrame = Frame()
+miFrame.config(width="700", height="400", bg="gray")
 miFrame.pack()
-
-Label(miFrame, text="Ingrese el texto para el analisis", fg="black", bg="lightblue").place(x=325, y=50, anchor="center")
+frameResult =  Frame()
+frameResult.config(width='700', height='400')
+frameResult.pack()
+Label(miFrame, text="Ingrese el texto para el analisis", fg="black", bg="gray").place(x=325, y=50, anchor="center")
 
 
 # TEXTAREA
 textCode = Text(miFrame, width=70, height=15, padx=10, pady=10)
 textCode.place(x=325, y=200, anchor="center")
-LabelResultado=Label(miFrame, text="Resultado: ", fg="black", bg="lightblue",font=("Arial Novas", 12))
-LabelResultado.place(x=325, y=500, anchor="center")
-
+LabelResultado= Label(frameResult, fg="black", bg = "white",font=("Arial Novas", 12))
+LabelResultado.place(x=325, y=200, anchor="center")
 
 # BOTONES
-botonLexico = Button(miFrame, text="Analizador Lexico",command=getTextInputLex).place(x=150, y=600)
-botonSintactico = Button(miFrame, text="Analizador Sintactico",command=getTextInputSin).place(x=400, y=600)
+botonLexico = Button(frameResult, text="Analizador Lexico",command=getTextInputLex).place(x=100, y=50)
+botonSintactico = Button(frameResult, text="Analizador Sintactico",command=getTextInputSin).place(x=400, y=50)
 
 
 raiz.mainloop()
